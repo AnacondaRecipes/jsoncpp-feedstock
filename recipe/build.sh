fda9dce23d7ff4c503698a70f389f7f329542840
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-10-21 15:25:57 -0500

    Bump version to 1.8.3; Simplify recipe for CB 3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
-if [ "$(uname)" == "Darwin" ]
-then
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-else
-    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
-
 mkdir build
 cd build
 cmake \
@@ -24,9 +9,12 @@ cmake \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DBUILD_STATIC_LIBS=1 \
   -DBUILD_SHARED_LIBS=1 \
-  -DPYTHON_EXECUTABLE="${PYTHON}" \
+  -DJSONCPP_WITH_TESTS=ON \
+  -DJSONCPP_WITH_POST_BUILD_UNITTEST=ON \
+  -DJSONCPP_WITH_PKGCONFIG_SUPPORT=ON \
+  -DJSONCPP_WITH_CMAKE_PACKAGE=ON \
+  -DPYTHON_EXECUTABLE="${SYS_PYTHON}" \
   ..
 
 make -j${CPU_COUNT}
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make jsoncpp_check
 make install
