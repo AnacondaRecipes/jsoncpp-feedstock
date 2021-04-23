275d80a813672f5e6b923e87b13175e1b01c3880
Serhii Kupriienko <skupriienko@anaconda.com>
2021-04-23 16:01:06 +0300

    Update meta.yaml, build.sh and bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,20 +2,21 @@ mkdir build && cd build
 
 set CMAKE_CONFIG=Release
 
+rem shared libs fail to build, built static instead
+rem https://github.com/open-source-parsers/jsoncpp/issues/631
+
 cmake ^
   -G "NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
-  -DBUILD_STATIC_LIBS=ON ^
   -DBUILD_SHARED_LIBS=ON ^
-  -DJSONCPP_WITH_TESTS=ON ^
-  -DJSONCPP_WITH_POST_BUILD_UNITTEST=ON ^
-  -DJSONCPP_WITH_PKGCONFIG_SUPPORT=ON ^
-  -DJSONCPP_WITH_CMAKE_PACKAGE=ON ^
-  -DPYTHON_EXECUTABLE="%SYS_PYTHON%" ^
+  -DPYTHON_EXECUTABLE="%PYTHON%" ^
   ..
 if errorlevel 1 exit 1
 
 cmake --build . --config %CMAKE_CONFIG% --target install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
+set -x
+
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
+if [ "$(uname)" == "Darwin" ]
+then
+    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+else
+    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
+
+export VERBOSE=1
+
 mkdir build
 cd build
-cmake \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
-  -DCMAKE_PREFIX_PATH="${PREFIX}" \
-  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-  -DBUILD_STATIC_LIBS=1 \
-  -DBUILD_SHARED_LIBS=1 \
-  -DJSONCPP_WITH_TESTS=ON \
-  -DJSONCPP_WITH_POST_BUILD_UNITTEST=ON \
-  -DJSONCPP_WITH_PKGCONFIG_SUPPORT=ON \
-  -DJSONCPP_WITH_CMAKE_PACKAGE=ON \
-  -DPYTHON_EXECUTABLE="${SYS_PYTHON}" \
-  ..
+for static_lib in "ON" "OFF" ; do
+  cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+    -DCMAKE_PREFIX_PATH="${PREFIX}" \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DBUILD_SHARED_LIBS=${static_lib} \
+    -DPYTHON_EXECUTABLE="${BUILD_PREFIX}/bin/python" \
+    -DJSONCPP_WITH_POST_BUILD_UNITTEST=off \
+    ..
+
+  make -j${CPU_COUNT}
+  if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make jsoncpp_check
+  fi
+  make install
+done
+
 
 make -j${CPU_COUNT}
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make jsoncpp_check
+fi
 make install
